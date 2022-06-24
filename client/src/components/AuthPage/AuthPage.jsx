229cf2a78f353a4f42a92cc8145ab6fe742e75ed
Fenvrir <dragonofhokage@gmail.com>
2022-06-24 23:49:36 +0700

    add hooks folder and new hook http.hook

--- client/src/components/AuthPage/AuthPage.jsx
+++ client/src/components/AuthPage/AuthPage.jsx
@@ -1,12 +1,53 @@
-import './AuthPage.scss';
-import React from 'react';
+import "./AuthPage.scss";
+import React, { useState } from "react";
+import { useHttp } from "../Hooks/Http.hook";
 
 const AuthPage = () => {
-    return (
-        <div >
-            
+  const [form, setForm] = useState({ email: "", password: "" });
+  const {loading, request, error} = useHttp();
+  const changeHandler = ev => {
+    setForm({...form, [ev.target.name]: ev.target.value});
+  }
+
+  return (
+    <div className="auth_form row">
+      <div className="col s6 offset-s3 ">
+        <div className="card grey lighten-2">
+          <div className="card-content black-text">
+            <p className="card-title">Авторизация</p>
+            <div className="input-field">
+              <input
+                name="email"
+                id="email"
+                type="email"
+                className="auth_inp validate"
+                onChange={changeHandler}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="input-field">
+              <input
+                name="password"
+                id="password"
+                type="password"
+                className="auth_inp validate"
+                onChange={changeHandler}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <div className="card-action ">
+            <button className="auth_form-btn btn waves-effect waves-light">
+              Login
+            </button>
+            <button className="auth_form-btn btn waves-effect waves-light">
+              Sign up
+            </button>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default AuthPage;
